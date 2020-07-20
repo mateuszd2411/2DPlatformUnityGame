4c3a6d8a2ed0222bb98a998f7ccc0a5673beeeaa
mateuszd2411 <mateuszd2411@gmail.com>
2020-07-20 13:21:19 +0200

    Added check if player is groun and can jump, set ground and player Layout

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,24 +8,34 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float jumpHeight;
 
+    public Transform groundCheck;
+    public float groundCheckRadious;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
-    
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadious, whatIsGround);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown (KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
-            GetComponent<Rigidbody2D>().velocity = 
+            GetComponent<Rigidbody2D>().velocity =
                 new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            GetComponent<Rigidbody2D>().velocity = 
+            GetComponent<Rigidbody2D>().velocity =
                 new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
 
@@ -34,8 +44,5 @@ public class PlayerController : MonoBehaviour
             GetComponent<Rigidbody2D>().velocity =
                 new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
-
-
-
     }
 }
