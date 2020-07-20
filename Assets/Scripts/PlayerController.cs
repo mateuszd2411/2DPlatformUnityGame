3ca3dcfce452775de5e42e8d9e6637c537b4b5c1
mateuszd2411 <mateuszd2411@gmail.com>
2020-07-20 10:57:04 +0200

    Adde some code for simple player moving and freeze Z rotation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().velocity = 
+                new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = 
+                new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity =
+                new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+
+
+    }
+}
