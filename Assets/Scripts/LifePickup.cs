77bd8099223762f6589720a89e32836cbd25d61c
mateuszd2411 <mateuszd2411@gmail.com>
2020-07-30 20:04:23 +0200

    Added add life hay item and set new HUD to another levels

---
+++ Assets/Scripts/LifePickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifePickup : MonoBehaviour
+{
+    private LifeManager lifeSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeSystem = FindObjectOfType<LifeManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        lifeSystem.GiveLife();
+        Destroy(gameObject);
+    }
+}
