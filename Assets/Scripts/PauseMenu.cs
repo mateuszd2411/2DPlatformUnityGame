b2bbf7b03a84d5afc1afbc19f72ea3c17ee3f90e
mateuszd2411 <mateuszd2411@gmail.com>
2020-07-30 14:30:15 +0200

    Pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public string levelSelected;
+
+    public string mainMenu;
+
+    public bool isPaused;
+
+    public GameObject pauseMenuCanvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPaused)
+        {
+            pauseMenuCanvas.SetActive(true);
+        }
+        else
+        {
+            pauseMenuCanvas.SetActive(false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+    }
+
+    public void Resuume()
+    {
+        isPaused = false;
+    }
+
+    public void LevelSelected()
+    {
+        Application.LoadLevel(levelSelected);
+    }
+
+    public void Quit()
+    {
+        Application.LoadLevel(mainMenu);
+    }
+    
+}
