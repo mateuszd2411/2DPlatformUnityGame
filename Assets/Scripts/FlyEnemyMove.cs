47a4a7a8b0173dcdda0cfea5c80b96ca413afefc
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-01 20:26:12 +0200

    Added mosqito enemy

---
+++ Assets/Scripts/FlyEnemyMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyEnemyMove : MonoBehaviour
+{
+    private PlayerController thePlayer;
+
+    public float moveSpeed;
+
+    public float playerRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position,
+            thePlayer.transform.position, moveSpeed * Time.deltaTime);
+    }
+}
