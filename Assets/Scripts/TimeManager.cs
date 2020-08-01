5a693a23ee0dc0dac89c52b06fb1591caa2f68ee
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-01 13:34:02 +0200

    Added World Timer

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public float startingTime;
+
+    private Text theText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theText = GetComponent<Text>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startingTime -= Time.deltaTime;
+
+        theText.text = "" + Math.Round(startingTime);
+    }
+}
