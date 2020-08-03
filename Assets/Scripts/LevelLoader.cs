f4676e631dc8cbf54838b26e18d87ef31281d181
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-03 19:15:34 +0200

    improved control buttons size

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -21,7 +21,7 @@ public class LevelLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxisRaw("Vertical") > 0.5f && playerInZone)
+        if(Input.GetAxisRaw("Jump") > 0.5f && playerInZone)
         {
             Application.LoadLevel(levelToLoad);
         }
