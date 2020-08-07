c169eb180167df0077ee901c4a138bda4bdb4dcb
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-07 20:55:55 +0200

    Added enemies

---
+++ Assets/Scripts/FallObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallObject : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name.Equals("Player"))
+        {
+            rigidbody2D.isKinematic = false;
+        }    
+    }
+}
---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+
+    public bool playerInRange;
+
+    public float playerRange;
+
+    public LayerMask palyerLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = FindObjectOfType<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInRange = Physics2D.OverlapCircle(transform.position, playerRange, palyerLayer);
+
+        if (!playerInRange)
+        {
+            audioSource.Play();
+        }
+    }
+}
