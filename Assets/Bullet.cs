9d7bc6417c2f50d16db3023560e1e52948b432d4
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-11 15:35:02 +0200

    Added shit for bird

---
+++ Assets/Bullet.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+
+    public PlayerController player;
+
+    private HealthManager theHealth;
+
+    //public GameObject enemyDeathEffect;
+
+    public GameObject impactEffect;
+
+    //public int pointsForKill;
+
+    public float rotationSpeed;
+
+    public int damageToGive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theHealth = FindObjectOfType<HealthManager>();
+
+        player = FindObjectOfType<PlayerController>();
+
+        /*if (player.transform.position.x < transform.position.x)
+        {
+            speed = -speed;
+            rotationSpeed = -rotationSpeed;
+        }*/
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.x);
+        GetComponent<Rigidbody2D>().angularVelocity = rotationSpeed;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.name == "Player")
+        {
+            //Instantiate(enemyDeathEffect, other.transform.position, other.transform.rotation);
+            //Destroy(other.gameObject);
+            //add points for kill enemy
+            //ScoreManager.AddPoints(pointsForKill);
+
+            HealthManager.HurtPlayer(damageToGive);
+        }
+
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
