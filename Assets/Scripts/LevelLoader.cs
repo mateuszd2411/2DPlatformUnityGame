7dc8d10fb8ef5db8614c10339001bbc9b219b208
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-12 20:29:35 +0200

    map

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -25,7 +25,7 @@ public class LevelLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxisRaw("Jump") > 0.5f && playerInZone)
+        if(Input.GetAxisRaw("Horizontal") > 0.5f && playerInZone)
         {
             //Application.LoadLevel(levelToLoad);
             LoadLevel();
