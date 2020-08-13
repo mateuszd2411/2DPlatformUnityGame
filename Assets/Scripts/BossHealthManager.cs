c836787ad95ed47753451e2b76437b1fe6dba6bd
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-13 13:37:48 +0200

    Boss level 5

--- Assets/Scripts/BossHealthManager.cs
+++ Assets/Scripts/BossHealthManager.cs
@@ -30,14 +30,14 @@ public class BossHealthManager : MonoBehaviour
 
             if(transform.localScale.y > minSize)
             {
-                GameObject clone1 = Instantiate(bossPrefab, new Vector3(transform.position.x + 2f, transform.position.y, transform.position.z), transform.rotation) as GameObject;
-                GameObject clone2 = Instantiate(bossPrefab, new Vector3(transform.position.x - 2f, transform.position.y, transform.position.z), transform.rotation) as GameObject;
+                GameObject clone1 = Instantiate(bossPrefab, new Vector3(transform.position.x + 3f, transform.position.y, transform.position.z), transform.rotation) as GameObject;
+                GameObject clone2 = Instantiate(bossPrefab, new Vector3(transform.position.x - 3f, transform.position.y, transform.position.z), transform.rotation) as GameObject;
 
-                clone1.transform.localScale = new Vector3(transform.localScale.y * 0.5f, transform.localScale.y * 0.5f, transform.localScale.z);
-                clone1.GetComponent<BossHealthManager>().enemyHealth = 2;
+                clone1.transform.localScale = new Vector3(transform.localScale.y * 0.6f, transform.localScale.y * 0.6f, transform.localScale.z);
+                clone1.GetComponent<BossHealthManager>().enemyHealth = 3;
 
-                clone2.transform.localScale = new Vector3(transform.localScale.y * 0.5f, transform.localScale.y * 0.5f, transform.localScale.z);
-                clone2.GetComponent<BossHealthManager>().enemyHealth = 2;
+                clone2.transform.localScale = new Vector3(transform.localScale.y * 0.6f, transform.localScale.y * 0.6f, transform.localScale.z);
+                clone2.GetComponent<BossHealthManager>().enemyHealth = 3;
             }
 
 
